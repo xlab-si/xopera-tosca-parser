38ccf47bc42b4a900fd3778f64e9afbbadbba613
Alexander Maslennikov <amaslenn@gmail.com>
2022-05-18 17:27:24 +0200

    Added docs and typing for methods

--- src/opera_tosca_parser/value.py
+++ src/opera_tosca_parser/value.py
@@ -1,11 +1,21 @@
 import copy
 
-from typing import Optional, Any
+from typing import Optional, Any, Dict
 
 from opera_tosca_parser.error import DataError
 
 
 class Value:
+    FUNCTIONS = frozenset((
+        "get_attribute",
+        "get_input",
+        "get_property",
+        "get_artifact",
+        "concat",
+        "join",
+        "token",
+    ))
+
     def __init__(self, typ: Optional, present: bool, data: Optional[Any] = None):
         """
         Construct Value object
@@ -39,27 +49,41 @@ class Value:
         """Overridden string representation"""
         return f"Value({self.present})[{self._data}]"
 
-    def dump(self):
+    def dump(self) -> Dict:
+        """
+        Dump data for Value object
+        :return: Data dict
+        """
         return dict(is_set=self.present, data=self._data)
 
-    def load(self, data):
+    def load(self, data: Dict):
+        """
+        Load data for Value object
+        :param data: Data dict
+
+        """
         self.present = data["is_set"]
         self._data = data["data"]
 
-    @property
-    def data(self):
-        if not self.present:
-            raise AssertionError("Accessing an unset value. Bug-fixing ahead ;)")
-        return self._data
-
-    def copy(self):
+    def copy(self) -> Any:
+        """
+        Copy Value object
+        :return: Copied Value object
+        """
         return type(self)(self.type, self.present, copy.deepcopy(self._data))
 
     def unset(self):
+        """
+        Reset Value object
+        """
         self._data = None
         self.present = False
 
-    def eval(self, instance, key):
+    def eval(self, instance: Any, key: str) -> Any:
+        """
+        Evaluate the data
+        :return: Data
+        """
         if not self.present:
             raise DataError(f"Cannot use an unset value: {key}")
 
@@ -81,21 +105,36 @@ class Value:
         return self.data
 
     @property
-    def is_function(self):
+    def is_function(self) -> bool:
+        """
+        Check whether this Value object is a TOSCA function
+        :return: True if it is function
+        """
         return Value.check_function(self.data)
 
     @staticmethod
-    def check_function(data):
+    def check_function(data) -> bool:
+        """
+        Check whether a Value object is a TOSCA function
+        :return: True if it is function
+        """
         return isinstance(data, dict) and len(data) == 1 and tuple(data.keys())[0] in Value.FUNCTIONS
 
     @staticmethod
-    def eval_function(data, instance):
+    def eval_function(data, instance: Any) -> Any:
+        """
+        Evaluate TOSCA function given the data
+        :return: Function value
+        """
         (function_name, params), = data.items()
         return getattr(instance, function_name)(params)
 
     @staticmethod
-    def check_eval_function(data, instance):
+    def check_eval_function(data, instance: Any) -> Any:
+        """
+        Check whether a Value object is a TOSCA function and evaluate function if it is
+        :return: Function value or data
+        """
         if Value.check_function(data):
             return Value.eval_function(data, instance)
         return data
-
