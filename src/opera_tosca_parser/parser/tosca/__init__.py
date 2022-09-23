a0fd31fa98b485d02f0c742e591f83706a05e690
anzoman <aluzarwork@gmail.com>
2022-09-23 20:55:50 +0200

    Enable parsing TOSCA version 1.3 along with 2.0

--- src/opera_tosca_parser/parser/tosca/__init__.py
+++ src/opera_tosca_parser/parser/tosca/__init__.py
@@ -1,17 +1,48 @@
 import importlib
 from pathlib import PurePath, Path
 
-from opera_tosca_parser.parser.tosca.v_1_3 import stdlib
 from opera_tosca_parser.error import ParseError
 from opera_tosca_parser.parser import yaml
-from opera_tosca_parser.parser.tosca.v_1_3 import Parser
-from opera_tosca_parser.parser.tosca.v_1_3 import ServiceTemplate
+from opera_tosca_parser.parser.tosca.v_1_3 import stdlib
+from opera_tosca_parser.parser.tosca.v_2_0 import Parser
+from opera_tosca_parser.parser.tosca.v_2_0 import ServiceTemplate
+from opera_tosca_parser.parser.tosca.v_2_0 import profiles
 from opera_tosca_parser.parser.utils.location import Location
 from opera_tosca_parser.parser.yaml.node import Node
 
-SUPPORTED_VERSIONS = dict(
-    tosca_simple_yaml_1_3="v_1_3"
+# TODO: Should we remove support for TOSCA v1.3 when TOSCA 2.0 officially becomes a standard?
+# This is a dict with name of TOSCA version as key and relative path to parser definitions as value
+SUPPORTED_TOSCA_VERSIONS = dict(
+    tosca_simple_yaml_1_3="v_1_3",
+    tosca_2_0="v_2_0"
 )
+
+
+def _get_tosca_version(input_yaml: Node) -> str:
+    """
+    Get TOSCA version for TOSCA service template
+    :param input_yaml: YAML Node input for TOSCA service template
+    :return: TOSCA version string
+    """
+    for k, v in input_yaml.value.items():
+        if k.value == "tosca_definitions_version":
+            try:
+                return SUPPORTED_TOSCA_VERSIONS[v.value]
+            except (TypeError, KeyError) as e:
+                raise ParseError(
+                    f"Unsupported TOSCA version. Available: {', '.join(SUPPORTED_TOSCA_VERSIONS.keys())}.", v.loc
+                ) from e
+
+    raise ParseError("Missing TOSCA version.", input_yaml.loc)
+
+
+def _get_parser(tosca_version: str) -> Parser:
+    """
+    Get parser for TOSCA service template
+    :param tosca_version: TOSCA version
+    :return: Parser object
+    """
+    return importlib.import_module(f".{tosca_version}", __name__).Parser  # type: ignore
 
 
 def load(base_path: Path, service_template: PurePath) -> ServiceTemplate:
@@ -29,36 +60,14 @@ def load(base_path: Path, service_template: PurePath) -> ServiceTemplate:
     tosca_version = _get_tosca_version(input_yaml)
     parser = _get_parser(tosca_version)
 
-    stdlib_yaml = stdlib.load(tosca_version)
-    service = parser.parse_service_template(stdlib_yaml, base_path, PurePath("STDLIB"), set())[0]
-    service.merge(parser.parse_service_template(input_yaml, base_path, service_template, set())[0])
+    if tosca_version == "v_2_0":
+        service = parser.parse_service_template(input_yaml, base_path, service_template, set())[0]
+    else:
+        stdlib_yaml = stdlib.load(tosca_version)
+        service = parser.parse_service_template(stdlib_yaml, base_path, PurePath("STDLIB"), set())[0]
+        service.merge(parser.parse_service_template(input_yaml, base_path, service_template, set())[0])
+
     service.visit("resolve_path", base_path)
     service.visit("resolve_reference", service)
 
     return service
-
-
-def _get_parser(tosca_version: str) -> Parser:
-    """
-    Get parser for TOSCA service template
-    :param tosca_version: TOSCA version
-    :return: Parser object
-    """
-    return importlib.import_module(f".{tosca_version}", __name__).Parser  # type: ignore
-
-
-def _get_tosca_version(input_yaml: Node) -> str:
-    """
-    Get TOSCA version for TOSCA service template
-    :param input_yaml: YAML Node input for TOSCA service template
-    :return: TOSCA version string
-    """
-    for k, v in input_yaml.value.items():
-        if k.value == "tosca_definitions_version":
-            try:
-                return SUPPORTED_VERSIONS[v.value]
-            except (TypeError, KeyError) as e:
-                raise ParseError(f"Invalid TOSCA version. Available: {', '.join(SUPPORTED_VERSIONS.keys())}.",
-                                 v.loc) from e
-
-    raise ParseError("Missing TOSCA version", input_yaml.loc)
